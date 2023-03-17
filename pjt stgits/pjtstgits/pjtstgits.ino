75c75117796e319e80bb20205523c8cda8c2008f
AravindMohan123 <2235@tkmce.ac.in>
2023-03-17 21:08:42 +0530

    elct iot v 1.0

---
+++ pjt stgits/pjtstgits/pjtstgits.ino
@@ -0,0 +1,239 @@
+#include <ESP8266WiFi.h>
+const int sensorIn = A0;      // pin where the OUT pin from sensor is connected on Arduino
+int mVperAmp = 66;           // this the 5A version of the ACS712 -use 100 for 20A Module and 66 for 30A Module
+int Watt = 0;
+double Voltage = 0;
+double VRMS = 0;
+double AmpsRMS = 0;
+
+// Replace with your network credentials
+const char* ssid     = "hippo";
+const char* password = "samurai123";
+
+// Set web server port number to 80
+WiFiServer server(80);
+
+// Variable to store the HTTP request
+String header;
+
+// Auxiliar variables to store the current output state
+String output5State = "off";
+String output4State = "off";
+
+// Assign output variables to GPIO pins
+const int output5 = 5;
+const int output4 = 4;
+
+// Current time
+unsigned long currentTime = millis();
+// Previous time
+unsigned long previousTime = 0; 
+// Define timeout time in milliseconds (example: 2000ms = 2s)
+const long timeoutTime = 2000;
+
+void setup() {
+  Serial.begin(115200);
+   
+  Serial.println ("ACS712 current sensor"); 
+  // Initialize the output variables as outputs
+  pinMode(output5, OUTPUT);
+  pinMode(output4, OUTPUT);
+  // Set outputs to LOW
+  digitalWrite(output5, LOW);
+  digitalWrite(output4, LOW);
+
+  // Connect to Wi-Fi network with SSID and password
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  // Print local IP address and start web server
+  Serial.println("");
+  Serial.println("WiFi connected.");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+  server.begin();
+}
+
+void loop(){
+ Serial.println (""); 
+  Voltage = getVPP();
+  VRMS = (Voltage/2.0) *0.707;   //root 2 is 0.707
+  AmpsRMS = ((VRMS * 1000)/mVperAmp)-0.3; //0.3 is the error I got for my sensor
+ 
+  Serial.print(AmpsRMS);
+  Serial.print(" Amps RMS  ---  ");
+  Watt = (AmpsRMS*230/1.2);
+  // note: 1.2 is my own empirically established calibration factor
+// as the voltage measured at D34 depends on the length of the OUT-to-D34 wire
+// 240 is the main AC power voltage – this parameter changes locally
+  Serial.print(Watt);
+  Serial.println(" Watts");
+  
+
+delay (100);  
+  WiFiClient client = server.available();   // Listen for incoming clients
+
+  if (client) {                             // If a new client connects,
+    Serial.println("New Client.");          // print a message out in the serial port
+    String currentLine = "";                // make a String to hold incoming data from the client
+    currentTime = millis();
+    previousTime = currentTime;
+    while (client.connected() && currentTime - previousTime <= timeoutTime) { // loop while the client's connected
+      currentTime = millis();         
+      if (client.available()) {             // if there's bytes to read from the client,
+        char c = client.read();             // read a byte, then
+        Serial.write(c);                    // print it out the serial monitor
+        header += c;
+        if (c == '\n') {                    // if the byte is a newline character
+          // if the current line is blank, you got two newline characters in a row.
+          // that's the end of the client HTTP request, so send a response:
+          if (currentLine.length() == 0) {
+            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
+            // and a content-type so the client knows what's coming, then a blank line:
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-type:text/html");
+            client.println("Connection: close");
+            client.println();
+            
+            // turns the GPIOs on and off
+            if (header.indexOf("GET /5/on") >= 0) {
+              Serial.println("BedRoom on");
+              output5State = "on";
+              digitalWrite(output5, HIGH);
+            } else if (header.indexOf("GET /5/off") >= 0) {
+              Serial.println("BedRoom off");
+              output5State = "off";
+              digitalWrite(output5, LOW);
+            } else if (header.indexOf("GET /4/on") >= 0) {
+              Serial.println("Hall on");
+              output4State = "on";
+              digitalWrite(output4, HIGH);
+            } else if (header.indexOf("GET /4/off") >= 0) {
+              Serial.println("Hall off");
+              output4State = "off";
+              digitalWrite(output4, LOW);
+            }
+            
+            // Display the HTML web page
+            client.println("<!DOCTYPE html><html>");
+            client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
+            client.println("<link rel=\"icon\" href=\"data:,\">");
+           client.println("<link rel='stylesheet' href='https://tradelink2022.000webhostapp.com/sas.css'");
+            // CSS to style the on/off buttons 
+            // Feel free to change the background-color and font-size attributes to fit your preferences
+           // client.println("<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
+            //client.println(".button { background-color: #195B6A; border: none; color: white; padding: 16px 40px;");
+            //client.println("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}");
+            //client.println(".button2 {background-color: #77878A;}</style></head>");
+            
+            // Web Page Heading
+            client.println("<body><h1>Electricity monitor v 1.0 pjt[stgits]</h1>");
+            
+            // Display current state, and ON/OFF buttons for GPIO 5  
+           // client.println("<p>Bedroom - State " + output5State+Watt + "</p>");
+            // client.println("<p> watts ::" + Watt + "</p>");
+              client.println("<p>Bedroom - State " + output5State + "</p>");
+                   client.println(" <p id='dod'></p>");                
+
+
+            // If the output5State is off, it displays the ON button       
+            if (output5State=="off") {
+              client.println("<p><a href=\"/5/on\"><button class=\"button\">ON</button></a></p>");
+            } else {
+              client.println("<p><a href=\"/5/off\"><button class=\"button button2\">OFF</button></a></p>");
+            } 
+               
+            // Display current state, and ON/OFF buttons for GPIO 4  
+            client.println("<p>Hall - State " + output4State + "</p>");
+            // If the output4State is off, it displays the ON button       
+            if (output4State=="off") {
+              client.println("<p><a href=\"/4/on\"><button class=\"button\">ON</button></a></p>");
+            } else {
+              client.println("<p><a href=\"/4/off\"><button class=\"button button2\">OFF</button></a></p>");
+            }
+            client.println("<marquee color='green'><u><h2>Sensor data<h2></u> </marquee>");
+            client.println("<table><tr><th>AmpsRMS");
+            client.println("</th><th>");     
+            client.println(AmpsRMS);  
+            client.println("</th></tr>");   
+
+            client.println("<tr><th>Watts");
+            client.println("</th><th>");     
+            client.println(Watt);  
+            client.println("</th></tr></table>");              
+            float c =0;
+            c=c+Watt;
+            client.println("<script>var a=0;window.onload = function(){setInterval(abc,1000);}");
+            client.println("function abc(){document.getElementById('dod').innerHTML=a");
+            client.println(c);
+            client.println(";}");
+            client.println("</script>");
+            client.println("</body></html>");
+            
+            // The HTTP response ends with another blank line
+            client.println();
+            // Break out of the while loop
+            break;
+          } else { // if you got a newline, then clear currentLine
+            currentLine = "";
+          }
+        } else if (c != '\r') {  // if you got anything else but a carriage return character,
+          currentLine += c;      // add it to the end of the currentLine
+        }
+      }
+    }
+    // Clear the header variable
+    header = "";
+    // Close the connection
+    client.stop();
+    Serial.println("Client disconnected.");
+    Serial.println("");
+  }
+}
+
+
+
+
+//elc
+
+
+// initialize the LCD library with I2C address and LCD size
+
+
+ 
+
+ 
+// ***** function calls ******
+float getVPP()
+{
+  float result;
+  int readValue;                // value read from the sensor
+  int maxValue = 0;             // store max value here
+  int minValue = 1023;          // store min value here ESP32 ADC resolution!!!
+  
+   uint32_t start_time = millis();
+   while((millis()-start_time) < 1000) //sample for 1 Sec
+   {
+       readValue = analogRead(sensorIn);
+       // see if you have a new maxValue
+       if (readValue > maxValue) 
+       {
+           /*record the maximum sensor value*/
+           maxValue = readValue;
+       }
+       if (readValue < minValue) 
+       {
+           /*record the minimum sensor value*/
+           minValue = readValue;
+       }
+   }
+   
+   // Subtract min from max
+   result = ((maxValue - minValue) * 3.3)/1023.0; //ESP32 ADC resolution 4096!!!
+      
+   return result;
+ }
